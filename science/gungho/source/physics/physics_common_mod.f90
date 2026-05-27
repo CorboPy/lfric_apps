6a5c342cd284fbc00e983f43b4516544e5c6ad66
Alex Corbett <alex.corbett48@gmail.com>
2026-05-27 11:59:17 +0100

    detailed log

--- science/gungho/source/physics/physics_common_mod.f90
+++ science/gungho/source/physics/physics_common_mod.f90
@@ -44,21 +44,26 @@ contains
     real(kind=r_def),  parameter :: qsa4 = 6.109_r_def       ! Constant in qsat equation in mbar
     real(kind=r_def), parameter :: small = 1.0e-6_r_def      ! Prevent division by very small numbers when p ~ es
 
+    character(len=128) :: msg
+
     if (T > qsa3) then
       es = qsa4 * exp(-qsa2 * (T - tk0c) / (T-qsa3))
 
       if (p > es) then
-        qs = epsilon * es / max(p - es, small) 
+        qs = epsilon * es / max(p - es, small)
       else
         ! Pressure below saturation vapour pressure
-        call log_event("qsaturation: p <= es", LOG_LEVEL_WARNING)
+        write(msg, '(A,ES16.8,A,ES16.8)') "qsaturation: p <= es, p=", p, ", es=", es
+        call log_event(msg, LOG_LEVEL_WARNING)
         qs = 999.0_r_def
       end if
     else
       ! Invalid temperature for Tetens
-      call log_event("qsaturation: T <= qsa3", LOG_LEVEL_WARNING)
+      write(msg, '(A,ES16.8,A,ES16.8)') "qsaturation: T <= qsa3, T=", T, ", qsa3=", qsa3
+      call log_event(msg, LOG_LEVEL_WARNING)
       qs = 999.0_r_def
     end if
   end function qsaturation
 
 end module physics_common_mod
+
